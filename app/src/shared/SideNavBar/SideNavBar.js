ff11c3f2a6d8b72c71eae7556ee8d0942f0a9066
Dan Hogue <chmod740@gmail.com>
2019-01-12 10:07:08 -0800

    Clean up nav styling rules

--- app/src/shared/SideNavBar/SideNavBar.js
+++ app/src/shared/SideNavBar/SideNavBar.js
@@ -6,26 +6,18 @@ export default class SideNavBar extends Component {
     static PropTypes = {
         cloudName: PropTypes.string.isRequired
     }
-    setActive(e) {
-        Array.from(document.getElementsByClassName('sidebar-link')).forEach(element => {
-            element.classList.remove("active")
-        });
-        e.currentTarget.classList.add("active")
-    }
     render() {
-        let basepath = '#/clouds/'+this.props.cloudName
+        const { cloudName } = this.props
+        let basepath = '#/clouds/' + cloudName
         return (
             <div id='side-menu-wrapper' className='navbar-default sidebar' role='navigation'>
                 <div className='sidebar-nav navbar-collapse'>
                     <ul id='side-menu' className='nav'>
                         <li title="Home"><a href={'/#'}><i className='fa fa-home fa-fw'></i></a></li>
                         <hr className='sidebar-section-devider'></hr>
-                        {/* <li title="Cloud Home"><a href={basepath+'/'} className="sidebar-link active" onClick={this.setActive}><i className='fa fa-th-large fa-fw'></i></a></li> */}
-                        <li title="DashBoards"><a href={basepath+'/boards'} className='sidebar-link active' onClick={this.setActive}><i className='fa fa-tachometer fa-fw'></i></a></li>
-                        {/* <hr className='sidebar-section-devider'></hr> */}
-                        <li title="DataFlows"><a href={basepath+'/flows'} className="sidebar-link" onClick={this.setActive}><i className='fa fa-share-alt fa-fw'></i></a></li>
-                        {/* <hr className='sidebar-section-devider'></hr> */}
-                        <li title="Users"><a href={basepath+'/users'} className="sidebar-link" onClick={this.setActive}><i className='fa fa-users fa-fw'></i></a></li>
+                        <li title="DashBoards"><a href={basepath+'/boards'} className='sidebar-link'><i className='fa fa-tachometer fa-fw'></i></a></li>
+                        <li title="DataFlows"><a href={basepath+'/flows'} className="sidebar-link"><i className='fa fa-share-alt fa-fw'></i></a></li>
+                        <li title="Users"><a href={basepath+'/users'} className="sidebar-link"><i className='fa fa-users fa-fw'></i></a></li>
                     </ul>
                 </div>
             </div>
