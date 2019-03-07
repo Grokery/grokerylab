3bd7cef73424b7e3260d50919f63578d7fd2b9a2
Dan Hogue <chmod740@gmail.com>
2019-03-07 07:56:55 -0800

    Bug fix and env update

--- app/src/config.js
+++ app/src/config.js
@@ -1,8 +1,7 @@
 
 
-export const API_BASE_URL = "http://localhost:8000/api/v0"
-// export const API_BASE_URL = "https://fn2u0hg7si.execute-api.us-west-2.amazonaws.com/dev/api/v0"
-
+// export const API_BASE_URL = "http://localhost:8000/api/v0"
+export const API_BASE_URL = "https://glab-api-dev.u4wtqp24pm.us-west-2.elasticbeanstalk.com/api/v0"
 
 export const headerNavHeight = 50
 export const sideNavWidth = 62
