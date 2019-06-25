01fb7b3d6eea5511253453266f9e359762f2792d
Daniel Hogue <dhogue@zulily.com>
2019-06-25 10:51:25 -0700

    point to prod by default

--- api/dockerrun.sh
+++ api/dockerrun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./dockerbuild.sh
+# ./dockerbuild.sh
 
-docker run --env-file ./environments/dev.env.config -d -p 8000:8000 grokerylab
+docker run --env-file ./environments/prod.env.config -d -p 8000:8000 grokerylab
--- api/run.sh
+++ api/run.sh
@@ -3,5 +3,5 @@
 mvn clean package
 
 # run spring
-export $(cat ./environments/dev.env.config | grep -v ^# | xargs)
+export $(cat ./environments/prod.env.config | grep -v ^# | xargs)
 java -jar target/grokerylab-api-spring-0.1.0.jar
